d4a6c718c202b942562c4bd8fe8a7d526ec38056
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2023-10-13 00:09:26 +0530

    Added new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,19 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Note App',
   description: 'Note App which can be shared online with flexible functionalities',
+  icons: {
+    icon: [{
+      media: "(prefers-color-scheme: light)",
+      url: "/1.png",
+      href:"/1.png"
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      url: "/2.png",
+      href:"/2.png"
+    }
+  ]
+  }
 }
 
 export default function RootLayout({
