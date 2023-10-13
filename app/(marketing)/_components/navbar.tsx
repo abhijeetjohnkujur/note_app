b1c2dd1aac0c180b5da726292c08c4c2c9edf529
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2023-10-13 16:23:48 +0530

    Added new packages navabar alteration etc

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,6 +3,7 @@
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const Navbar = () => {
 
@@ -14,7 +15,7 @@ const Navbar = () => {
     )}>
         <Logo />
         <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-            Login
+            <ModeToggle />
         </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -28,8 +29,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange storageKey='note-app-theme'>
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   )
 }
