c1acaed4c924598e4139cab948e97c7ae713b555
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2023-10-13 17:55:30 +0530

    Loading ui added

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,9 +4,14 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 const Navbar = () => {
 
+    const { isAuthenticated, isLoading } = useConvexAuth()
     const scrolled = useScrollTop()
 
   return (
@@ -15,6 +20,24 @@ const Navbar = () => {
     )}>
         <Logo />
         <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+          {isLoading&& (
+            <Spinner />
+          )}
+          {!isAuthenticated && !isLoading && (
+            <>
+            <SignInButton mode="modal">
+                <Button size={"sm"} >
+                  Get Note App Free
+                </Button>
+              </SignInButton>
+
+              <SignInButton mode="modal">
+                <Button variant={"ghost"} size={"sm"} >
+                  Log in
+                </Button>
+              </SignInButton>
+            </>
+          )}
             <ModeToggle />
         </div>
     </div>
