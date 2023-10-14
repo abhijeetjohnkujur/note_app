7fd0da37de2bc52cf6fbb2a9e5987d4d40ad1278
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2023-10-14 13:36:34 +0530

    Added some user settings and appended other small changes

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 const DocumentsPage = () => {
+    const {user} = useUser()
     return (
-        <div>
-            Docuements Page
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image
+            src="/empty.svg"
+            height="300"
+            width="300"
+            alt="Empty Image"
+            />  
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos;s Note App
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2" />
+                Create a Note
+            </Button>
         </div>
     )
 }
