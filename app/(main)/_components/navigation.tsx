f617e76d941da1657059f13399a520d61f54a645
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2023-10-14 11:26:03 +0530

    Added some correction

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { ElementRef,useRef, useState } from "react";
+import { ElementRef,useEffect,useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 const Navigation = () => {
@@ -14,6 +14,20 @@ const Navigation = () => {
     const navbarRef = useRef<ElementRef<"div">>(null)
     const [isResetting,setIsResetting] = useState(false)
     const [isCollapsed,setIsCollapsed] = useState(isMobile)
+
+    useEffect(() => {
+        if(isMobile) {
+            collapse()
+        } else {
+            resetWidth()
+        }
+    },[isMobile])
+
+    useEffect(() =>{
+        if(isMobile){
+            collapse()
+        }
+    },[pathname, isMobile])
 
     const handleMouseDown = (
         event: React.MouseEvent<HTMLDivElement, MouseEvent>
