9c672f0c7937b804206c2c7655a5e06a6248e17e
abhijeetjohnkujur <abhijeetkujur000@gmail.com>
2023-10-18 08:45:25 +0530

    Added some cosmetics

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef,useEffect,useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -129,6 +129,17 @@ const Navigation = () => {
         </div>
         <div>
             <UserItem />
+            <Item 
+            label="Search"
+            icon={Search}
+            isSearch
+            onClick={() => {}}
+            />
+            <Item 
+            label="Settings"
+            icon={Settings}
+            onClick={() => {}}
+            />
             <Item onClick={handleCreate} label="New Page" icon={PlusCircle}/>
         </div>
 
